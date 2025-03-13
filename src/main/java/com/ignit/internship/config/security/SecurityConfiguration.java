cf45ec9b6d540b5c94c0c5d0aa10ac5d1da25e3a
WhateverAWS <151141957+WhateverAWS@users.noreply.github.com>
2025-03-13 13:48:37 +0700

    `fix:` update security config to accept patch method

--- src/main/java/com/ignit/internship/config/security/SecurityConfiguration.java
+++ src/main/java/com/ignit/internship/config/security/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
